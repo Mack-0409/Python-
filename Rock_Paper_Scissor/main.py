7e392ced65445c860050b8ae18437c9ff55f47ce
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-20 23:54:10 +0530

    Rock_Paper_Scissor

--- Rock_Paper_Scissor/main.py
+++ Rock_Paper_Scissor/main.py
@@ -1,3 +1,4 @@
+
 import random
 item_list = ["Rock", "Paper", "Scissor"]
 
@@ -6,3 +7,26 @@ user_choice = input("enter our move = Rock, Paper, Scissor = ")
 comp_choice = random.choice(item_list)
 
 print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
+
+if user_choice == comp_choice:
+    print("It's a tie!")
+elif user_choice == "Rock":
+    if comp_choice == "Paper":
+        print("paper covers rock")
+    else:
+        print("Rock smashes scissor")
+
+elif user_choice == "Paper":
+    if comp_choice == "Scissor":
+        print("scissor cuts paper")
+    else:
+        print("paper covers rock")
+
+elif user_choice == "Scissor":
+    if comp_choice == "Paper":
+        print("scissor cuts paper")
+    else :
+        print("rock smashes scissor")
+
+
+ 
