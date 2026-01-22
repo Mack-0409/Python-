6a88590f2f068a75ee4f2330c6bc3041f9787150
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-22 23:36:53 +0530

    Mini_Restaurent

---
+++ Mini_Restaurent/main.py
@@ -0,0 +1,14 @@
+# Define the menu of restaurant
+menu = {
+    'Pizza':40,
+    'Pasta':50,
+    'Burger':60,
+    'Salad':70,
+    'Coffee':80,
+}
+
+# Greet
+print("Welcome to PYTHON Restaurant")
+print("pizza:40rs\nPasta:50rs\nBurger':60rs\nSalad':70rs\nCoffee':80rs")
+
+ 
