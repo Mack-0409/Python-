a13502600aa9c6a22f37d368ec406f92ae9d4402
Mack-0409 <mayanksingh0409@gmail.com>
2026-01-22 23:43:46 +0530

    Mini_Restaurant

--- Mini_Restaurent/main.py
+++ Mini_Restaurent/main.py
@@ -11,4 +11,13 @@ menu = {
 print("Welcome to PYTHON Restaurant")
 print("pizza:40rs\nPasta:50rs\nBurger':60rs\nSalad':70rs\nCoffee':80rs")
 
- 
+order_total = 0
+
+item_1 = input("Enter the name of item you want o order =  ")
+if item_1 in menu:
+    order_total = menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+    
+else :
+        
+     
